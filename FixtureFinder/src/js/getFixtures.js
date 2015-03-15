cf725b5c843a54f0750d3885e7884e4bd1151fc1
Phil Quinn <abetterway2feel@hotmail.co.uk>
2015-03-15 23:44:42 +0100

    Added Jasmin test for parseFixture JSON

--- FixtureFinder/src/js/getFixtures.js
+++ FixtureFinder/src/js/getFixtures.js
@@ -1,49 +1,45 @@
-function getFixturesByDate(date) {
-    var url = 'http://localhost:8080/fixtures/byDate?callback=?';
-
-    $.ajax({
-     type: 'GET',
-     data: { "date": today() },
-     url: url,
-     async: false,
-     jsonpCallback: 'jsonCallback',
-     contentType: "application/json",
-     dataType: 'jsonp',
-     success: function(json) {
-         populateFixturse(json.fixtures);
-     },
-     error: function(json) {
-         console.log(json.messages);
-     }
- });
-}
-
-function populateFixturse(fixtures){
-    $.each(fixtures, function(index, fixture ) {
-            $('.fixtures > .elements').append(getFixture(fixture));
-    });
-}
-
-function getFixture(fixture){
-    var listElement = '<li class="fixture">';
-    listElement = listElement + '<span class="kickOffDate">' + fixture.kickOff.date + '</span>';
-    listElement = listElement + '<span class="kickOffTime">' + fixture.kickOff.time + '</span>';
-    listElement = listElement + '<span class="home team">' + fixture.homeTeam +'</span>';
-    listElement = listElement + '<span class="vs">'+fixture.score+'</span>';
-    listElement = listElement + '<span class="away team">' + fixture.awayTeam +'</span>';
-    listElement = listElement + '</li>';
-    return listElement;
-}
-
-function initialize(){
-    getFixturesByDate(today());
-}
-
-function today(){
-    return new Date().getFullYear()
-    + "-" + (new Date().getMonth() + 1)
-    + "-" + (new Date().getDate() )
-}
-
-$(document).ready( initialize() );
-
+var FixtureRetriever = {
+    getFixturesByDate: function(date){ 
+        var url = 'http://localhost:8080/fixtures/byDate?callback=?';
+        $.ajax({
+           type: 'GET',
+           data: { "date": date },
+           url: url,
+           async: false,
+           jsonpCallback: 'jsonCallback',
+           contentType: "application/json",
+           dataType: 'jsonp',
+           success: function(json) {
+               FixtureParser.parseFixtures(json.fixtures);
+           },
+           error: function(json) {
+               console.log(json.messages);
+           }
+       });
+    }
+};
+var FixtureParser = {  
+    parseFixtures: function(fixtures){
+        $.each(fixtures, function(index, fixture ) {
+            $('.fixtures > .elements').append(FixtureParser.getFixtureAsHTMLElement(fixture));
+        });
+    },
+    getFixtureAsHTMLElement: function(fixture){
+        var listElement = '<li class="fixture">';
+        listElement = listElement + '<span class="kickOffDate">' + fixture.kickOff.date + '</span>';
+        listElement = listElement + '<span class="kickOffTime">' + fixture.kickOff.time + '</span>';
+        listElement = listElement + '<span class="home team">' + fixture.homeTeam +'</span>';
+        listElement = listElement + '<span class="vs">'+fixture.score+'</span>';
+        listElement = listElement + '<span class="away team">' + fixture.awayTeam +'</span>';
+        listElement = listElement + '</li>';
+        return listElement;
+    },
+    initialize: function(){
+        FixtureRetriever.getFixturesByDate( FixtureParser.today() );
+    },
+    today: function(){
+        return new Date().getFullYear()
+        + "-" + (new Date().getMonth() + 1)
+        + "-" + (new Date().getDate() )
+    }
+};
---
+++ FixtureFinder/src/js/init.js
@@ -0,0 +1,3 @@
+$(document).ready( 
+	FixtureParser.initialize()
+);
